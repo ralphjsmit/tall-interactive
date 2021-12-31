9350bd1e7dee1ab9e2ac307c0d754d0e45b9cd87
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2021-12-31 17:23:36 +0100

    Update ActionablesServiceProvider.php

--- src/ActionablesServiceProvider.php
+++ src/ActionablesServiceProvider.php
@@ -5,7 +5,7 @@ namespace RalphJSmit\Tall\Interactive;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class SkeletonServiceProvider extends PackageServiceProvider
+class ActionablesServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
