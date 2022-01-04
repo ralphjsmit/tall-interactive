e52687d60c09e9151f507860fe77005911438aa4
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-01-04 18:23:08 +0100

    Move TestClasses to separate file

---
+++ tests/Fixtures/TestClasses.php
@@ -0,0 +1,59 @@
+<?php
+
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Testing\Assert;
+use RalphJSmit\Tall\Interactive\Forms\Form;
+
+class TestForm extends Form
+{
+    public static int $submittedTimes = 0;
+
+    public static function getFormSchema(): array
+    {
+        return [
+            TextInput::make('email')->label('Enter your e-mail')->required(),
+        ];
+    }
+
+    public static function getFormDefaults(): array
+    {
+        return [
+            'email' => '',
+            'year' => 2000,
+        ];
+    }
+
+    public static function submitForm(array $formData, object|null $record): void
+    {
+        static::$submittedTimes++;
+        Assert::assertNull($record);
+    }
+}
+
+class User extends Model
+{
+    protected $guarded = [];
+}
+
+class UserForm extends Form
+{
+    public static $expectedUser;
+
+    public static \Closure $assertionCallable;
+
+    public static function getFormSchema(): array
+    {
+        return [];
+    }
+
+    public static function getFormDefaults(): array
+    {
+        return [];
+    }
+
+    public static function submitForm(array $formData, object|null $record): void
+    {
+        Assert::assertInstanceOf(User::class, $record);
+    }
+}
--- tests/Pest.php
+++ tests/Pest.php
@@ -3,5 +3,9 @@
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use RalphJSmit\Tall\Interactive\Tests\TestCase;
 
-uses(TestCase::class, LazilyRefreshDatabase::class)
-    ->in(__DIR__);
+require __DIR__ . '/Fixtures/TestClasses.php';
+
+uses(
+    TestCase::class,
+    LazilyRefreshDatabase::class,
+)->in(__DIR__);
