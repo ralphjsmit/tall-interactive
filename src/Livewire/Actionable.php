ff30e09b65509f785725ac34dd0d5654b02556ab
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-01-05 17:16:13 +0100

    Integrate with toasts

--- src/Livewire/Actionable.php
+++ src/Livewire/Actionable.php
@@ -3,9 +3,12 @@
 namespace RalphJSmit\Tall\Interactive\Livewire;
 
 use Livewire\Component;
+use Usernotnull\Toast\Concerns\WireToast;
 
 abstract class Actionable extends Component
 {
+    use WireToast;
+
     protected $listeners = [];
 
     public function close()
