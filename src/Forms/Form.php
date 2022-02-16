857822110e78fd030862a3f1fb634ef5780dbafc
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-02-16 10:22:31 +0100

    Make form wireable

--- src/Forms/Form.php
+++ src/Forms/Form.php
@@ -2,6 +2,25 @@
 
 namespace RalphJSmit\Tall\Interactive\Forms;
 
-abstract class Form /* implements ToForm */
+use Livewire\Wireable;
+
+abstract class Form implements Wireable
 {
+    public function __construct(array $properties = [],)
+    {
+        dump($properties);
+        foreach ($properties as $property => $value) {
+            $this->{$property} = $value;
+        }
+    }
+
+    public static function fromLivewire($value): self
+    {
+        return new static($value);
+    }
+
+    public function toLivewire(): array
+    {
+        return get_object_vars($this);
+    }
 }
