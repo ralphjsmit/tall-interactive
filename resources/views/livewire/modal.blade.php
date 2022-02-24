f7f4cf894cbacf92e8cb3f88ed9d66170d5025e0
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-02-24 11:49:19 +0100

    Add dark mode support to the modal

--- resources/views/livewire/modal.blade.php
+++ resources/views/livewire/modal.blade.php
@@ -27,13 +27,13 @@
             <!-- This element is to trick the browser into centering the modal contents. -->
             <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
-            <div class="inline-block align-bottom bg-white rounded-lg text-left shadow-xl transform transition-all sm:my-8 sm:align-middle {{ $maxWidth }} sm:w-full"
+            <div class="inline-block align-bottom bg-white text-black dark:bg-gray-800 dark:text-white rounded-lg text-left shadow-xl transform transition-all sm:my-8 sm:align-middle {{ $maxWidth }} sm:w-full"
                  x-show="state"
                  x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
                  x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
                 @if($dismissable)
-                    <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4 rounded-[inherit]">
+                    <div class="hidden sm:block absolute top-0 right-0 pt-4 pr-4">
                         <button type="button" wire:click="$emit('modal:close', '{{ $actionableId }}')" class="bg-none hover:bg-gray-100 p-1 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
                             <span class="sr-only">{{ $dismissableWith }}</span>
 
@@ -45,10 +45,10 @@
                         </button>
                     </div>
                 @endif
-                <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 rounded-[inherit]">
+                <div class="px-4 pt-5 pb-4 sm:p-6 sm:pb-4 rounded-t-[inherit]">
                     <div class="">
                         @if($title)
-                            <h3 class="text-lg leading-6 font-bold text-gray-900" id="modal-title">
+                            <h3 class="text-lg leading-6 font-bold text-gray-900 dark:text-white" id="modal-title">
                                 {{ $title }}
                             </h3>
                         @endif
@@ -81,8 +81,9 @@
                     </div>
                 </div>
                 @if($showControls)
-                    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse rounded-[inherit]" id="tall-interactive-slide-over-controls">
-                        <button wire:click="{{ $formClass ? 'submitForm': (!$formClass && $slot ? 'submitSlot' : '') }}" type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary-600 text-base font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2
+                    <div class="bg-gray-50 dark:bg-gray-800 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse rounded-b-[inherit]" id="tall-interactive-slide-over-controls">
+                        <button wire:click="{{ $formClass ? 'submitForm': (!$formClass && $slot ? 'submitSlot' : '') }}" type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary-600 text-base font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-2
+                        focus:ring-offset-2
                     focus:ring-primary-500 sm:ml-3
                     sm:w-auto
                     sm:text-sm">
@@ -93,7 +94,9 @@
 
                         @if($dismissable)
                             <button wire:click="$emit('modal:close', '{{ $actionableId }}')" type="button"
-                                    class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                                    class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 dark:border-gray-600 shadow-sm px-4 py-2 bg-white dark:bg-gray-700 dark:text-white text-base font-medium text-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+                                    sm:mt-0 sm:ml-3 sm:w-auto
+                                    sm:text-sm">
                                 {{ $dismissableWith }}
                             </button>
                         @endif
