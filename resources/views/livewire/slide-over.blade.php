7b9822c3d8e110d34be459213b2cedfba4056610
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-03-17 16:30:27 +0100

    Update slide-over.blade.php

--- resources/views/livewire/slide-over.blade.php
+++ resources/views/livewire/slide-over.blade.php
@@ -103,7 +103,7 @@
 
                                     @include('tall-interactive::components.forms.button-actions', ['buttonActions' => $this->getButtonActions()])
 
-                                    <button type="submit" wire:click="{{ $formClass ? 'submit': (!$formClass && $slot ? 'submitSlot' : '') }}"
+                                    <button type="submit" wire:click="{{ $formClass ? 'submit': 'submitSlot' }}"
                                             class="w-full md:w-auto inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
                                         {{ $submitWith }}
                                     </button>
