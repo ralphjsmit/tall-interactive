b76ce2ff1962453c2138a4b837bae5aa3ad3e194
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-03-17 16:31:17 +0100

    Update inline-form.blade.php

--- resources/views/livewire/inline-form.blade.php
+++ resources/views/livewire/inline-form.blade.php
@@ -55,7 +55,7 @@
                 <x-dynamic-component :component="'tall-interactive::forms.controls.' . $controlsDesign">
                     @include('tall-interactive::components.forms.button-actions', ['buttonActions' => $this->getButtonActions()])
 
-                    <button wire:click="{{ $formClass ? 'submit': (!$formClass && $slot ? 'submitSlot' : '') }}" type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary-600 text-base font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2
+                    <button wire:click="{{ $formClass ? 'submit': 'submitSlot' }}" type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary-600 text-base font-medium text-white hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2
                     focus:ring-primary-500 sm:ml-3
                     sm:w-auto
                     sm:text-sm">
