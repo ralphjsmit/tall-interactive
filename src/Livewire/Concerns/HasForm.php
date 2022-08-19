038d56b15b2d5b5ad7508ed45842f12c7495b79d
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-08-19 13:21:06 +0200

    Fix

--- src/Livewire/Concerns/HasForm.php
+++ src/Livewire/Concerns/HasForm.php
@@ -16,7 +16,7 @@ trait HasForm
     use InteractsWithForms;
     use RegisterListeners;
 
-    public array $data = [];
+    public ?array $data = [];
 
     public bool $hasConstructedForm = false;
 
@@ -26,11 +26,11 @@ trait HasForm
 
     public function bootedHasForm(): void
     {
-        if ($this->shouldFillForm && $this->formClass) {
+        if ( $this->shouldFillForm && $this->formClass ) {
             $this->mountForm();
         }
 
-        if ($this->formClass) {
+        if ( $this->formClass ) {
             $this->registerFormMessages();
         }
     }
@@ -40,7 +40,7 @@ trait HasForm
         collect($this->call('getButtonActions'))
             ->reject(fn (ButtonAction $buttonAction) => $buttonAction->isHidden())
             ->each(function (ButtonAction $buttonAction) use ($buttonActionName): void {
-                if ($buttonAction->getName() !== $buttonActionName) {
+                if ( $buttonAction->getName() !== $buttonActionName ) {
                     return;
                 }
 
@@ -76,7 +76,7 @@ trait HasForm
                     $this->makeForm()
                         ->schema($this->call('getFormSchema') ?: [])
                         ->tap(function (ComponentContainer $componentContainer): ComponentContainer {
-                            if ($this->model) {
+                            if ( $this->model ) {
                                 $componentContainer->model($this->model);
                             }
 
@@ -92,7 +92,7 @@ trait HasForm
     {
         $this->handleCloseOnSubmit();
 
-        if (! $this->model) {
+        if ( ! $this->model ) {
             $this->reset('data', 'shouldFillForm');
 
             $this->mountForm();
