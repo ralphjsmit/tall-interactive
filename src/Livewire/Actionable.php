3a7b93cacf1d36dc88b571024594b76c26930e96
Ralph J. Smit <59207045+ralphjsmit@users.noreply.github.com>
2022-09-09 18:25:27 +0200

    Update Actionable.php

--- src/Livewire/Actionable.php
+++ src/Livewire/Actionable.php
@@ -3,12 +3,9 @@
 namespace RalphJSmit\Tall\Interactive\Livewire;
 
 use Livewire\Component;
-use Usernotnull\Toast\Concerns\WireToast;
 
 abstract class Actionable extends Component
 {
-    use WireToast;
-
     protected $listeners = [
         '$refresh',
     ];
