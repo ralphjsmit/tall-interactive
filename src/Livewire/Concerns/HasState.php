1ff001ec9ce95da27d9e4e80cc6e9bbd62cc3922
Ching Cheng Kang <pycck@hotmail.com>
2022-12-16 11:26:12 +0800

    make $actionable accept null

--- src/Livewire/Concerns/HasState.php
+++ src/Livewire/Concerns/HasState.php
@@ -8,8 +8,12 @@ trait HasState
 
     public bool $actionableOpen = false;
 
-    public function closeActionable(string $actionable): void
+    public function closeActionable(?string $actionable): void
     {
+        if (!$actionable) {
+            retrun;
+        }
+        
         if ($this->actionableId !== $actionable) {
             return;
         }
