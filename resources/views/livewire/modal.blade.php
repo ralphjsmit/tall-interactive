e2bd31857f3088227d09a07728d984c44acbb5fa
Sydney Hill <sydneyehill@gmail.com>
2023-01-17 13:14:48 -0800

    Class hooks for modal, inline-form, and slide-over
    Signed-off-by: Sydney Hill <sydneyehill@gmail.com>

--- resources/views/livewire/modal.blade.php
+++ resources/views/livewire/modal.blade.php
@@ -15,7 +15,7 @@
 
 <div class="relative z-[999]" id="{{ $actionableId }}">
     {{-- Modal --}}
-    <div class="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" x-cloak
+    <div class="tall-interactive-modal fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true" x-cloak
          x-data="{ state: $wire.entangle('actionableOpen') }" :class="state ? 'pointer-events-auto' : 'pointer-events-none'">
         <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:p-0 sm:block">
             <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"
@@ -27,7 +27,7 @@
             <!-- This element is to trick the browser into centering the modal contents. -->
             <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
-            <div class="tall-interactive-modal inline-block align-bottom bg-white text-black dark:bg-gray-800 dark:text-white rounded-lg text-left shadow-xl transform transition-all sm:my-8 sm:align-middle {{ $maxWidth }} sm:w-[calc(100%-24px)]"
+            <div class="tall-interactive-modal-body inline-block align-bottom bg-white text-black dark:bg-gray-800 dark:text-white rounded-lg text-left shadow-xl transform transition-all sm:my-8 sm:align-middle {{ $maxWidth }} sm:w-[calc(100%-24px)]"
                  x-show="state"
                  x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
                  x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
